2c42f59d8ced00d6780d7b438ac70db5ed94dd70
Simon CHANU <simon.chanu@laposte.net>
2021-07-04 11:23:16 +0200

    change game to activity

--- Models.py
+++ Models.py
@@ -42,7 +42,7 @@ class EventModel:
         Args:
             persons (List[str]): Liste de noms (uniques) des personnes de l'evenement
             slots_nb (int): Nombre de slots de temps disponibles
-            game_list (List[str]): Liste des noms (uniques) des jeux disponibles
+            activity_list (List[str]): Liste des noms (uniques) des jeux disponibles
             max_parallel (int): nombre d'activitées maximum en simultané sur un slot
         """
         
@@ -59,28 +59,28 @@ class EventModel:
         self.max_parallel = max_parallel
         self.max_preference = np.inf
 
-    def get_games_preference_score(self, games: List[str], exclude_players=[]):
+    def get_activities_preference_score(self, activities: List[str], exclude_players=[]):
         # On exclu les jeux dont un des exclus est GM
-        excluded_games = self.activities[self.activities['org'].isin(exclude_players)].index
-        games_serie = pd.Series(games)
-        games = games_serie[~games_serie.isin(excluded_games)].tolist()
+        excluded_activities = self.activities[self.activities['org'].isin(exclude_players)].index
+        activities_serie = pd.Series(activities)
+        activities = activities_serie[~activities_serie.isin(excluded_activities)].tolist()
         # On exclu les mises des joueurs exclu
         # print("exclude_players=", exclude_players)
-        # print(self.preferences[~self.preferences.index.isin(exclude_players)][games])
+        # print(self.preferences[~self.preferences.index.isin(exclude_players)][activities])
 
-        sum_chosen = self.preferences[~self.preferences.index.isin(exclude_players)][games].sum(axis=0)
-        sum_excluded = self.preferences[self.preferences.index.isin(exclude_players)][games].sum(axis=0)
+        sum_chosen = self.preferences[~self.preferences.index.isin(exclude_players)][activities].sum(axis=0)
+        sum_excluded = self.preferences[self.preferences.index.isin(exclude_players)][activities].sum(axis=0)
 
         return sum_chosen - sum_excluded
         # return sum_chosen
 
-    def get_best_players(self, game, exclude_players=[]):
+    def get_best_players(self, activity, exclude_players=[]):
         # Serie des joueurs ayant le plus misé sur le jeu
         df = self.preferences[~self.preferences.index.isin(exclude_players)]
 
         # les best_players sont tout les joueurs dont la mise maximale est sur le jeu
         # Serie si pour chacun des joueur c'est sa meilleure mise
-        best_player_serie = df[(df[game] == df.max(axis=1))&(df.max(axis=1) != 0)][game]
+        best_player_serie = df[(df[activity] == df.max(axis=1))&(df.max(axis=1) != 0)][activity]
    
         best_players = []
         if not best_player_serie.empty:
@@ -90,17 +90,17 @@ class EventModel:
 
         return best_players
         
-    def get_best_players_in(self, games, exclude_players=[]) -> List[str]:
+    def get_best_players_in(self, activities, exclude_players=[]) -> List[str]:
         """On prends parmis les 2 jeux la mise la plus forte et on l'applique
 
         Args:
-            games (List[str]): jeux à intégrer dans le calcul
+            activities (List[str]): jeux à intégrer dans le calcul
             exclude_players (List[str], optional): liste de joueurs à écarter du calcul. Defaults to [].
 
         Returns:
             List[str]: [description]
         """
-        df = self.preferences[~self.preferences.index.isin(exclude_players)][games]
+        df = self.preferences[~self.preferences.index.isin(exclude_players)][activities]
         max_value = df.max().max()
 
         players_df = df[df[:] == max_value].dropna(how="all", inplace=False)
@@ -109,8 +109,8 @@ class EventModel:
             rand.shuffle(players)
 
         for i, p in enumerate(players):
-            wanted_games = players_df.loc[p,:][players_df.loc[p,:] == max_value].index.tolist()
-            players[i] = [p, wanted_games]
+            wanted_activities = players_df.loc[p,:][players_df.loc[p,:] == max_value].index.tolist()
+            players[i] = [p, wanted_activities]
 
         return players
 
@@ -155,8 +155,8 @@ class EventModel:
             print("ERROR : a preference is not positive")
             # print(self.preferences)
             pers_test = self.preferences[self.preferences < 0].any(axis=1)
-            game_test = self.preferences[self.preferences < 0].any(axis=0)
-            print(self.preferences.loc[pers_test.values, game_test.values])
+            activity_test = self.preferences[self.preferences < 0].any(axis=0)
+            print(self.preferences.loc[pers_test.values, activity_test.values])
 
             return False
         
@@ -188,87 +188,90 @@ class EventInstance:
         self.model: EventModel = model
 
         # self.slots: List[SlotInstance] = []
-        self.games_states: DataFrame = DataFrame(index=self.model.activities.index, columns=['planned', 'available', 'full', 'score'])
-        self.game_slots_states: DataFrame = DataFrame(index=self.model.slots.columns, columns=['full_people', 'full_activity', 'index_to_fill', 'available_players_nb_min', 'available_players_nb_max'])
+        self.activities_states: DataFrame = DataFrame(index=self.model.activities.index, columns=['planned', 'available', 'full', 'score'])
+        self.activity_slots_states: DataFrame = DataFrame(index=self.model.slots.columns, columns=['full_people', 'full_activity', 'index_to_fill', 'available_players_nb_min', 'available_players_nb_max'])
 
         # out
         self.plan_activities: DataFrame = DataFrame(index=range(self.model.max_parallel), columns=self.model.slots.columns)
         self.plan_persons: DataFrame = DataFrame(index=self.model.persons, columns=self.model.slots.columns)
 
         # init 
-        self.init_games_states()
-        self.init_game_slots_states()
+        self.init_activities_states()
+        self.init_activity_slots_states()
 
-    def init_games_states(self):
-        self.games_states.loc[:,'planned'] = False
-        self.games_states.loc[:,'available'] = True
-        self.games_states.loc[:,'full'] = False
-        self.games_states.loc[:,'score'] = 0
+    def init_activities_states(self):
+        self.activities_states.loc[:,'planned'] = False
+        self.activities_states.loc[:,'available'] = True
+        self.activities_states.loc[:,'full'] = False
+        self.activities_states.loc[:,'score'] = 0
 
-    def init_game_slots_states(self):
-        self.game_slots_states.loc[:,'full_people'] = False
-        self.game_slots_states.loc[:,'full_activity'] = False
-        self.game_slots_states.loc[:,'index_to_fill'] = 0
-        self.game_slots_states.loc[:,'available_players_nb_min'] = len(self.model.persons)
-        self.game_slots_states.loc[:,'available_players_nb_max'] = len(self.model.persons)
-        # print(self.game_slots_states)
+    def init_activity_slots_states(self):
+        self.activity_slots_states.loc[:,'full_people'] = False
+        self.activity_slots_states.loc[:,'full_activity'] = False
+        self.activity_slots_states.loc[:,'index_to_fill'] = 0
+        self.activity_slots_states.loc[:,'available_players_nb_min'] = len(self.model.persons)
+        self.activity_slots_states.loc[:,'available_players_nb_max'] = len(self.model.persons)
+        # print(self.activity_slots_states)
 
-    def get_nb_players_left_in_slot_for_game(self, slot, game):
+    def get_nb_players_left_in_slot_for_activity(self, slot, activity):
         """ On recupere le nombre de personne restantes maximum qu'on peut mettre sur une table sans invalider
         les autres par manque de joueurs.
 
         Args:
             slot (int): [description]
-            game (str): [description]
+            activity (str): [description]
 
         Returns:
             [type]: [description]
         """
-        # On recupere la liste des jeux du slot (sauf game)
-        other_games = self.plan_activities[slot][self.plan_activities[slot] != game][self.plan_activities[slot].notnull()].tolist()
-        nb_people_already_in_game = self.plan_persons[slot][self.plan_persons[slot] == game].count()
-        if len(other_games)>0:
-            min_people_other_games = self.model.activities['min_people'][other_games].sum() + len(other_games)
+        # On recupere la liste des jeux du slot (sauf activity)
+        other_activities = self.plan_activities[slot][self.plan_activities[slot] != activity][self.plan_activities[slot].notnull()].tolist()
+        nb_people_already_in_activity = self.plan_persons[slot][self.plan_persons[slot] == activity].count()
+        if len(other_activities)>0:
+            min_people_other_activities = self.model.activities['min_people'][other_activities].sum() + len(other_activities)
             return min(
-                self.model.slots[slot].value_counts()[1] - min_people_other_games - nb_people_already_in_game, 
-                self.model.activities.loc[game, 'max_people'] - nb_people_already_in_game + 1
+                self.model.slots[slot].value_counts()[1] - min_people_other_activities - nb_people_already_in_activity, 
+                self.model.activities.loc[activity, 'max_people'] - nb_people_already_in_activity + 1
             )
         else:
             return min(
                 self.model.slots[slot].value_counts()[1], 
-                self.model.activities.loc[game, 'max_people'] - nb_people_already_in_game + 1
+                self.model.activities.loc[activity, 'max_people'] - nb_people_already_in_activity + 1
             ) 
 
-    def get_available_games(self):
-        return self.games_states[self.games_states['available'] == True].index.tolist()
+    def get_available_activities(self):
+        return self.activities_states[self.activities_states['available'] == True].index.tolist()
 
-    def update_game_availablility(self, unavailable_players):
+    def update_activity_availablility(self, unavailable_players):
         # On retire les jeux des GM déjà non disponibles
-        games_unavailable_players = self.model.activities[self.model.activities['org'].isin(unavailable_players)].index.tolist()
+        activities_unavailable_players = self.model.activities[self.model.activities['org'].isin(unavailable_players)].index.tolist()
         
-        # TODO il faudrait un objet slot qui possede son gamestate pour garder les infos et pas les recalculer
+        # TODO il faudrait un objet slot qui possede son activitystate pour garder les infos et pas les recalculer
         # On recalcule les jeux non disponibles a partir du planning
-        games_unavailable_planning = self.plan_activities.stack().unique()
-        # print("games_unavailable_planning=", games_unavailable_planning)
+        activities_unavailable_planning = self.plan_activities.stack().unique()
+        # print("activities_unavailable_planning=", activities_unavailable_planning)
 
-        self.games_states['available'] = True
-        self.games_states.loc[games_unavailable_players, 'available'] = False
-        self.games_states.loc[games_unavailable_planning, 'available'] = False
+        self.activities_states['available'] = True
+        self.activities_states.loc[activities_unavailable_players, 'available'] = False
+        self.activities_states.loc[activities_unavailable_planning, 'available'] = False
 
-    def add_game_to_slot(self, game, slot):
+    def add_activity_to_slot(self, activity, slot):
         # add activity to plan
-        self.plan_activities.loc[self.game_slots_states.loc[slot,'index_to_fill'], slot] = game
-        self.game_slots_states.loc[slot,'index_to_fill'] += 1
+        self.plan_activities.loc[self.activity_slots_states.loc[slot,'index_to_fill'], slot] = activity
+        self.activity_slots_states.loc[slot,'index_to_fill'] += 1
         
-        # add gm to pla persons
-        gm = self.model.activities.loc[game,'org']
-        self.plan_persons.loc[gm, slot] = game
+        # add gm to plan persons
+        gm = self.model.activities.loc[activity,'org']
 
-        # game not available
-        self.games_states.loc[game, 'available'] = False
+        # Ignore activities without org
+        if gm != np.nan and gm != '' and gm != None:
+            self.plan_persons.loc[gm, slot] = activity
+
+        # activity not available
+        self.activities_states.loc[activity, 'available'] = False
 
 
-    def get_best_game(self, slot, by='score') -> str:
+    def get_best_activity(self, slot, by='score') -> str:
         # On enleve les jeux qui ont un nombre de joueur minimum trop élevé pour rentrer dans le slot
         nb_max_players_left_in_slot = self.get_theo_nb_players_left_in_slot(slot, by='min_people')
 
@@ -277,27 +280,27 @@ class EventInstance:
         if len(activities_filtered) == 0:
             return NaN
 
-        best_game = self.games_states.loc[activities_filtered, 'score'].idxmax()
+        best_activity = self.activities_states.loc[activities_filtered, 'score'].idxmax()
         
-        return best_game
+        return best_activity
 
-    def get_games_sorted(self, games, by='score') -> List[str]:
-        return self.games_states.loc[games,:].sort_values(by=by, axis=0, ascending=False, inplace=False).index.tolist() 
+    def get_activities_sorted(self, activities, by='score') -> List[str]:
+        return self.activities_states.loc[activities,:].sort_values(by=by, axis=0, ascending=False, inplace=False).index.tolist() 
 
-    def update_games_score(self, games, exclude_players=[]) -> None:
-        self.games_states.loc[:,'score'] = self.model.get_games_preference_score(games, exclude_players=exclude_players)
+    def update_activities_score(self, activities, exclude_players=[]) -> None:
+        self.activities_states.loc[:,'score'] = self.model.get_activities_preference_score(activities, exclude_players=exclude_players)
 
-    def get_full_games_from_slot(self, slot: int) -> List[str]:
+    def get_full_activities_from_slot(self, slot: int) -> List[str]:
         # On cherche les jeux dont le nombre de personne dans le slot a atteint son maximum
-        games_in_slot = self.get_games_from_slot(slot)
+        activities_in_slot = self.get_activities_from_slot(slot)
 
         # Pour chacun de ces jeux on regarde si leur nombre de joueur atteint son maximum
-        games_full = []
-        for game in games_in_slot:
-            if(self.plan_persons[slot][self.plan_persons[slot] == game].count() > self.model.activities.loc[game, 'max_people']):
-                games_full.append(game)
+        activities_full = []
+        for activity in activities_in_slot:
+            if(self.plan_persons[slot][self.plan_persons[slot] == activity].count() > self.model.activities.loc[activity, 'max_people']):
+                activities_full.append(activity)
 
-        return games_full
+        return activities_full
 
     def get_unavailable_players(self, slot: int) -> List[str]:
         unavailable_players = self.model.slots[slot][self.model.slots[slot] == 0].index.tolist()
@@ -319,23 +322,23 @@ class EventInstance:
         self.plan_activities.to_csv(base+"_plan_activities"+ext)
         self.plan_persons.to_csv(base+"_plan_persons"+ext)
 
-    def get_games_from_slot(self, slot):
+    def get_activities_from_slot(self, slot):
         return self.plan_activities[slot][self.plan_activities[slot].notnull()].tolist()
 
-    def are_game_slots_full_activities(self):
+    def are_activity_slots_full_activities(self):
         res = (
-            (self.game_slots_states['full_activity'].all()) # Vérifie si les slots sont plein (plus de joueurs dispo)
-            or (not self.games_states['available'].any()) # Vérifie si l'état interne indique que c'est full
+            (self.activity_slots_states['full_activity'].all()) # Vérifie si les slots sont plein (plus de joueurs dispo)
+            or (not self.activities_states['available'].any()) # Vérifie si l'état interne indique que c'est full
             or (not self.plan_activities.isnull().any().any()) # Vérifie si il reste des jeux de disponibles
         )
         return res
 
-    def are_game_slot_full_persons(self, slot):
-        games = self.get_games_from_slot(slot)
+    def are_activity_slot_full_persons(self, slot):
+        activities = self.get_activities_from_slot(slot)
         nb_persons_busy = self.plan_persons[slot].count()
 
         print("nb_persons_busy=", nb_persons_busy)
-        print("max_people_in_games=", self.model.activities[self.model.activities.index.isin(games)]['max_people'].sum() + len(games))
+        print("max_people_in_activities=", self.model.activities[self.model.activities.index.isin(activities)]['max_people'].sum() + len(activities))
         
         # print("slot=", slot, "self.model.slots=", self.model.slots)
         # print("value_counts=\n", self.model.slots[slot])
@@ -344,7 +347,7 @@ class EventInstance:
             (nb_persons_busy == self.model.slots[slot].value_counts()[1]) # Vérifie si il reste des gens de disponibles
             or (
                 nb_persons_busy 
-                >= self.model.activities[self.model.activities.index.isin(games)]['max_people'].sum() + len(games)
+                >= self.model.activities[self.model.activities.index.isin(activities)]['max_people'].sum() + len(activities)
             ) # Il n'y a plus de place sur les jeux
         )
 
@@ -366,7 +369,7 @@ class EventInstance:
         return nb_min_players_left_in_slot <= 0
 
     def set_slot_full_with_activities(self, slot):
-        self.game_slots_states.loc[slot, 'full_activity'] = True
+        self.activity_slots_states.loc[slot, 'full_activity'] = True
 
 
 ### test
--- Optimizer.py
+++ Optimizer.py
@@ -41,11 +41,11 @@ class Optimizer:
         df_happyness = pd.DataFrame()
         for pers in event.model.persons:
             df_pref_pers = df_pref.loc[pers, :]
-            games_serie_person_planned = event.plan_persons.loc[pers, :]
+            activities_serie_person_planned = event.plan_persons.loc[pers, :]
 
-            games_serie_person_not_planned = df_pref_pers[~df_pref_pers.index.isin(games_serie_person_planned)].index
+            activities_serie_person_not_planned = df_pref_pers[~df_pref_pers.index.isin(activities_serie_person_planned)].index
 
-            df_pref.loc[pers, games_serie_person_not_planned] = df_pref.loc[pers, games_serie_person_not_planned].multiply(-1, fill_value=0)
+            df_pref.loc[pers, activities_serie_person_not_planned] = df_pref.loc[pers, activities_serie_person_not_planned].multiply(-1, fill_value=0)
 
         df_happyness = df_pref.sum(axis=1)
         # print(df_pref)
@@ -59,61 +59,61 @@ class OptimizerDeterminist(Optimizer):
     def __init__(self) -> None:
         super().__init__()
 
-    def insert_best_game_in_slot(self, event: EventInstance, slot: SlotInstance):
+    def insert_best_activity_in_slot(self, event: EventInstance, slot: SlotInstance):
         # Joueurs qui sur ce slot sont déjà dans un jeu ou sont le gm d'un autre sont à enlever du choix
         unavailable_players = event.get_unavailable_players(slot)
 
         # Pour les joueurs unavailable on met leur jeu en unavailable
-        event.update_game_availablility(unavailable_players)
-        games = event.get_available_games()
+        event.update_activity_availablility(unavailable_players)
+        activities = event.get_available_activities()
 
-        # Somme les mises du modele selon les arguments sur les lignes et les place dans games_states
-        event.update_games_score(games, exclude_players=unavailable_players)
+        # Somme les mises du modele selon les arguments sur les lignes et les place dans activities_states
+        event.update_activities_score(activities, exclude_players=unavailable_players)
 
         # On recupere la liste ordonnee des jeux en fonction de leur score
-        best_game = event.get_best_game(slot, by='score')
+        best_activity = event.get_best_activity(slot, by='score')
 
-        if isinstance(best_game, str):
-            print("Add game", best_game, "to plan in slot", slot)
-            event.add_game_to_slot(best_game, slot)
+        if isinstance(best_activity, str):
+            print("Add activity", best_activity, "to plan in slot", slot)
+            event.add_activity_to_slot(best_activity, slot)
             # Si le dernier jeu peut mettre tout le monde sur une seule activité alors on considere le slot full aussi
             if event.are_activities_in_slot_sufficient(slot):
                 print("Sufficient, end slot", slot)
                 event.set_slot_full_with_activities(slot)       
 
             # Pour optimiser on met le joueur le plus intéressé par la table dessus (en cas d'égalité il y a de l'aleatoire)
-            best_players = event.model.get_best_players(best_game, exclude_players=unavailable_players)
+            best_players = event.model.get_best_players(best_activity, exclude_players=unavailable_players)
             if len(best_players)>0:
                 print("best_players=",best_players)
 
                 # recupere le nombre de joueur maximum que peut acceuillir la table
-                max_players_nb = event.model.activities['min_people'][best_game]
+                max_players_nb = event.model.activities['min_people'][best_activity]
                 for i in range(min(max_players_nb, len(best_players))):
-                    event.plan_persons.loc[best_players[i], slot] = best_game
+                    event.plan_persons.loc[best_players[i], slot] = best_activity
 
             else: 
                 print("No best player available : bet more on another table")                     
         else: 
             # On ne peut plus trouver de jeu pour ce slot : il est full
             event.set_slot_full_with_activities(slot)
-            print("No more games for slot", slot)
+            print("No more activities for slot", slot)
 
     def insert_best_people_in_slot(self, event, slot):        
         # Pour chaque table dans le slot
         unavailable_players = event.get_unavailable_players(slot)
         print("unavailable_players=", unavailable_players)
         
-        # unavailable_games = event.get_full_games_from_slot(slot)
-        # print("unavailable_games=", unavailable_games)
+        # unavailable_activities = event.get_full_activities_from_slot(slot)
+        # print("unavailable_activities=", unavailable_activities)
 
         # On prends la personne avec la plus grande mise parmis les 2 tables du slot
-        games = event.get_games_from_slot(slot)
+        activities = event.get_activities_from_slot(slot)
 
         # On retire les jeux dejà plein
-        excluded_game = set(event.games_states[event.games_states['full']].index.tolist())
-        games = list(set(games) - excluded_game)
+        excluded_activity = set(event.activities_states[event.activities_states['full']].index.tolist())
+        activities = list(set(activities) - excluded_activity)
 
-        best_players = event.model.get_best_players_in(games, exclude_players=unavailable_players)
+        best_players = event.model.get_best_players_in(activities, exclude_players=unavailable_players)
 
         # Il faut que le nombre max de joueur rajoutable sur la table ne depasse pas
         # (le nombre de joueurs total) - (somme des min des autres tables)
@@ -122,16 +122,16 @@ class OptimizerDeterminist(Optimizer):
             # Gestion des mises égales
             if len(p[1])>1:
                 # On prend le jeu qui a actuellement le plus de place restante
-                games_players_left = [(event.get_nb_players_left_in_slot_for_game(slot, game), game) for game in p[1]]
-                p[1] = max(games_players_left, key=lambda x: x[0])[1]
+                activities_players_left = [(event.get_nb_players_left_in_slot_for_activity(slot, activity), activity) for activity in p[1]]
+                p[1] = max(activities_players_left, key=lambda x: x[0])[1]
             else:
                 p[1] = p[1][0]
 
-            nb_players_left = event.get_nb_players_left_in_slot_for_game(slot, p[1])
+            nb_players_left = event.get_nb_players_left_in_slot_for_activity(slot, p[1])
             if(nb_players_left>0):
                 event.plan_persons.loc[p[0], slot] = p[1]
             else:
-                event.games_states.loc[p[1], 'full'] = True
+                event.activities_states.loc[p[1], 'full'] = True
 
     def fill_slots_from_preferences(self, event: EventInstance):
         """Utilise les préférences des personnes de l'evenement pour mettre en place 
@@ -140,7 +140,7 @@ class OptimizerDeterminist(Optimizer):
 
         # Remplissage des activités dans les slots
         depth = 0
-        while not event.are_game_slots_full_activities():
+        while not event.are_activity_slots_full_activities():
             # On somme les mise sur une activité et on les classe de manière décroissante
             if depth % 2 == 0:
                 slot_list = event.plan_activities.columns.tolist()
@@ -149,7 +149,7 @@ class OptimizerDeterminist(Optimizer):
 
             for slot_id in slot_list:
                 print("=== FILL ACTIVITY LOOP :", slot_id)
-                self.insert_best_game_in_slot(event, slot_id)
+                self.insert_best_activity_in_slot(event, slot_id)
             
             depth+=1
 
@@ -158,16 +158,16 @@ class OptimizerDeterminist(Optimizer):
         
         # Pour chaque slot
         for slot in event.plan_activities:
-            while not event.are_game_slot_full_persons(slot):
+            while not event.are_activity_slot_full_persons(slot):
                 # if slot == '180':
                 #     print("DEBUG")
                 print("=== FILL PEOPLE LOOP :", slot)
                 self.insert_best_people_in_slot(event, slot)
 
-        # print(event.game_slots)
-        # print(games_sorted_by_score)
-        # print(event.are_game_slots_full())
-        # for slot in event.game_slots.values():
+        # print(event.activity_slots)
+        # print(activities_sorted_by_score)
+        # print(event.are_activity_slots_full())
+        # for slot in event.activity_slots.values():
         #     print(slot, slot.available_players_nb_min, slot.available_players_nb_max, slot.is_full)
 
     def optimize(self, event: EventModel):
